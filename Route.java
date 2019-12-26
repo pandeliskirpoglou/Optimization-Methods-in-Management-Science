0ca4298639caff86f4f5edf0d596cae7705ea910
pandeliskirpoglou <44750498+pandeliskirpoglou@users.noreply.github.com>
2019-12-26 12:01:50 +0200

    Create Route.java

---
+++ Route.java
@@ -0,0 +1,23 @@
+package project_mebede;
+
+import java.util.ArrayList;
+
+
+
+public class Route {
+		 	
+			ArrayList<Node> nodes = new ArrayList();
+	        double cost;
+	        double load;
+	        double capacity;
+	        
+	        public Route(double cap) {
+	            cost = 0;
+	            nodes = new ArrayList();
+	            load = 0;
+	            capacity = cap;
+	        
+	
+	}
+
+}
