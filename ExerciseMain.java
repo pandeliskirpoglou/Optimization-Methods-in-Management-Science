2a820a07f69e559ee69db3bb9d1b919927f45431
pandeliskirpoglou <44750498+pandeliskirpoglou@users.noreply.github.com>
2019-12-26 11:35:58 +0200

    Update ExerciseMain.java

--- ExerciseMain.java
+++ ExerciseMain.java
@@ -1,10 +1,15 @@
 package project_mebede;
 
+
 public class ExerciseMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+        VRP vrp = new VRP(100, 1500, 1200);
+        vrp.generateNetworkRandomly();
+		
+		
 	}
 
 }
